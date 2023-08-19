8d7cae1ca4d7f5be9c0b072f8f25c25440579173
toheriman <e2h3in@gmail.com>
2023-08-19 12:12:08 +0200

    Run lock script

--- hardhat/contracts/Lock.sol
+++ hardhat/contracts/Lock.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract Lock {
     uint public unlockTime;
@@ -10,20 +8,18 @@ contract Lock {
 
     event Withdrawal(uint amount, uint when);
 
-    constructor(uint _unlockTime) payable {
+    constructor(address _owner, uint _unlockTime) payable {
         require(
             block.timestamp < _unlockTime,
             "Unlock time should be in the future"
         );
 
         unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+        owner = payable(_owner);
     }
 
     function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
         require(msg.sender == owner, "You aren't the owner");
 
--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -3,19 +3,21 @@ import { ethers } from "hardhat";
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
-
   const lockedAmount = ethers.parseEther("0.001");
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
+  // Specify the desired owner's address here
+  const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+
+  const lock = await ethers.deployContract("Lock", [ownerAddress, unlockTime], {
     value: lockedAmount,
   });
 
   await lock.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `Lock with ${ethers.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${
+      lock.target
+    }. And ${lock}. And owner is ${ownerAddress}`
   );
 }
 
