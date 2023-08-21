1e4dfe6035a0abb0c705d7e8e20530a1585944f9
toheriman <e2h3in@gmail.com>
2023-08-21 16:56:52 +0200

    Deployment task and script

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -11,36 +11,28 @@ const config: HardhatUserConfig = {
 
 export default config;
 
-task("compileAndUpdate", "Compiles the contracts and updates the Next.js app")
+task("dev", "Compiles, deploys, and updates using the localhost network")
   .addParam("contract", "Contract name")
   .setAction(async ({ contract }, hre) => {
+    // Set the network to localhost
+    hre.network.name = "localhost";
+
+    // Compile the contract
     await hre.run("compile");
 
-    const contractArtifact = await hre.artifacts.readArtifact(contract);
-    const outputPath = path.join(__dirname, "..", "next", "contracts", `${contract}.json`);
-    fs.writeFileSync(outputPath, JSON.stringify(contractArtifact, null, 2));
-
-    console.log(`ABI for ${contract} updated in the Next.js app.`);
-  });
-
-task("deployAndUpdate", "Deploys the contract and updates the Next.js app")
-  .addParam("contract", "Contract name")
-  .setAction(async ({ contract }, hre) => {
+    // Deploy the contract
     const contractDeployed = await hre.ethers.deployContract(contract);
     await contractDeployed.waitForDeployment();
     const address = await contractDeployed.getAddress();
 
+    // Write the ABI to the Next.js app
+    const contractArtifact = await hre.artifacts.readArtifact(contract);
+    const outputPath = path.join(__dirname, "..", "next", "contracts", `${contract}.json`);
+    fs.writeFileSync(outputPath, JSON.stringify(contractArtifact, null, 2));
+
+    // Write the contract address to the Next.js app
     const contractAddressPath = path.join(__dirname, "..", "next", "contracts", `ContractAddress.json`);
     fs.writeFileSync(contractAddressPath, JSON.stringify({ address }));
 
-    console.log(`${contract} deployed to ${address} and updated in the Next.js app.`);
-  });
-
-task("dev", "Compiles, deploys, and updates using the localhost network")
-  .addParam("contract", "Contract name")
-  .setAction(async ({ contract }, hre) => {
-    hre.network.name = "localhost";
-    await hre.run("compileAndUpdate", { contract: contract });
-    await hre.run("deployAndUpdate", { contract: contract });
-    console.log(`Tasks completed for ${contract} on localhost.`);
+    console.log(`Tasks completed for ${contract} on localhost. Deployed to ${address} and updated in the Next.js app.`);
   });
--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,16 +1,20 @@
 import { ethers } from "hardhat";
 
-async function main() {
+async function main(contractName: string): Promise<string> {
   const contract = await ethers.deployContract("FoodWars");
 
   await contract.waitForDeployment();
   const address = await contract.getAddress();
-  console.log(`FoodWars with  deployed to ${contract.target}. And ${JSON.stringify(contract)}. And owner is ${address}`);
-}
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.log(`${contractName} deployed to ${address}.`);
+  return address;
+}
+const contractName = process.argv[2];
+main(contractName)
+  .then((address) => {
+    console.log(address); // This will print the address to stdout
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
